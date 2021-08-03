245ee950fc8b772059409ec0a1359038ee519e77
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-03 17:19:08 -0400

    add folder for appointments component and add to stories

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "components/Appointments/styles.scss";
+
+export default function Appointments() {
+  return (
+    <article className="appointment"></article>
+  );
+}
