376e29c94e2f3e2b3c816e7c1bdf8edda6487c80
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-02 20:33:25 -0400

    test day list item to ensure it renders the correct message depending on number of spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,21 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   })
 
+  //Function to render correct message depending on props.spots
+  const formatSpots = function(spots) {
+    let number;
+    let noun;
+
+    spots === 0 ? number = "no" : number = `${spots}`;
+    spots === 1 ? noun = "spot" : noun = "spots";
+
+    return `${number} ${noun}`;
+  }
+
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
