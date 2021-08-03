5a345cd875df06e910ebf31de546c84219fae39a
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-03 15:05:44 -0400

    add interviewer list item component and stories with styling

--- stories/index.js
+++ stories/index.js
@@ -8,7 +8,11 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
 
+
+
+//Button stories
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -26,6 +30,7 @@ storiesOf("Button", module)
   ));
 
 
+  //Day list item stories
   storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -38,6 +43,7 @@ storiesOf("Button", module)
   ));
 
 
+  //Day list stories
   const days = [
     {
       id: 1,
@@ -67,3 +73,37 @@ storiesOf("Button", module)
       <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
     ));
 
+//Interviewer List Item stories
+    const interviewer = {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    };
+    
+    storiesOf("InterviewerListItem", module)
+      .addParameters({
+        backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+      })
+      .add("Unselected", () => (
+        <InterviewerListItem
+          id={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+        />
+      ))
+      .add("Selected", () => (
+        <InterviewerListItem
+          id={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          selected
+        />
+      ))
+      .add("Clickable", () => (
+        <InterviewerListItem
+          id={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          setInterviewer={action("setInterviewer")}
+        />
+      ));
