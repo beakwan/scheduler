ac39a59b58af76926572f4bc033660b080adfd0c
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-03 15:49:42 -0400

    build and style interviewer list with stories

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,35 @@
+// PROPS 
+// interviewers: array 
+// interviewer: number 
+// setInterviewer: function 
+
+import React from "react";
+
+import InterviewerListItem from "components/InterviewerListItem";
+
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+
+  const { interviewers } = props;
+
+  const parsedInterviewers = interviewers.map(interviewer => {
+    return <InterviewerListItem  
+    key={interviewer.id} 
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected={interviewer.id === props.interviewer}
+    setInterviewer={event => props.setInterviewer(interviewer.id)} />
+  })
+  
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {parsedInterviewers}
+      </ul>
+    </section>
+  );
+  
+}
--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
 
 
 
@@ -73,7 +74,8 @@ storiesOf("Button", module)
       <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
     ));
 
-//Interviewer List Item stories
+
+    //Interviewer List Item stories
     const interviewer = {
       id: 1,
       name: "Sylvia Palmer",
@@ -107,3 +109,31 @@ storiesOf("Button", module)
           setInterviewer={action("setInterviewer")}
         />
       ));
+
+
+      //Interviewer list stories
+      const interviewers = [
+        { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+        { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+        { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+        { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+        { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+      ];
+      
+      storiesOf("InterviewerList", module)
+        .addParameters({
+          backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+        })
+        .add("Initial", () => (
+          <InterviewerList
+            interviewers={interviewers}
+            setInterviewer={action("setInterviewer")}
+          />
+        ))
+        .add("Preselected", () => (
+          <InterviewerList
+            interviewers={interviewers}
+            interviewer={3}
+            setInterviewer={action("setInterviewer")}
+          />
+        ));
