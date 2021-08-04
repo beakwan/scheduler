fd417c643773fd7003ddda8034d3bbf2629d11c4
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-04 13:35:38 -0400

    add state and proper save and cancel functionality to form

--- src/components/Appointments/Form.js
+++ src/components/Appointments/Form.js
@@ -16,15 +16,27 @@ export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = function() {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = function() {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder={props.name ? props.name : "Enter student name"}
+            placeholder="Enter student name"
+            value={name}
+            onChange={event => setName(event.target.value)}
           
           />
         </form>
@@ -32,8 +44,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={event => props.onSave(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
