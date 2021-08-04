c3641985957c40f397e5a2db4ccfe797671c9a83
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-04 13:52:01 -0400

    update index to include header, show, and empty states

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import Header from "components/Appointments/Header";
+import Empty from "components/Appointments/Empty";
+import Show from "components/Appointments/Show";
+
+
 import "components/Appointments/styles.scss";
 
-export default function Appointments() {
+export default function Appointments(props) {
+  const {time, interview} = props;
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+    </article>
   );
 }
