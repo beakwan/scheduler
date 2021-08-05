88b84b6f6cc1d61539cc4c058984561150dd855c
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-05 19:47:41 -0400

    add history state to add transition and back functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,17 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  return { mode };
+  const [history, setHistory] = useState([initial]);
+  
+  const transition = (newMode) => {
+    history.push(newMode);
+    setMode(history[history.length - 1]);
+  }
+  
+  const back = () => {
+    history.pop();
+    setMode(history[history.length - 1]);
+  }
+  
+  return { mode, transition, back };
 }
