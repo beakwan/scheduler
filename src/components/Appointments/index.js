9204ebd49be151d72511867866469e2c34a840b2
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-05 20:26:32 -0400

    add transition functionality from empty to form and back

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import Header from "components/Appointments/Header";
 import Empty from "components/Appointments/Empty";
 import Show from "components/Appointments/Show";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 
 import "components/Appointments/styles.scss";
@@ -10,10 +12,31 @@ import "components/Appointments/styles.scss";
 export default function Appointment(props) {
   const {time, interview} = props;
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          {...interview}
+          interviewers ={[]}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 }
