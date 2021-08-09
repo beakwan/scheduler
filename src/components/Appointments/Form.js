ed28a648b6ce4b02c5003e93d2bba9ea3750c003
Beatrice <beatrice.c.kwan@gmail.com>
2021-08-09 17:49:13 -0400

    use tests to add error message if user tries to submit blank form

--- src/components/Appointments/Form.js
+++ src/components/Appointments/Form.js
@@ -17,6 +17,7 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = function() {
     setName("");
@@ -26,6 +27,15 @@ export default function Form(props) {
   const cancel = function() {
     reset();
     onCancel();
+  }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    onSave(name, interviewer);
   }
 
   return (
@@ -39,15 +49,18 @@ export default function Form(props) {
             placeholder="Enter student name"
             value={name}
             onChange={event => setName(event.target.value)}
+            
+            data-testid="student-name-input"
           
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList interviewers={interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={() => validate()}>Save</Button>
         </section>
       </section>
     </main>
